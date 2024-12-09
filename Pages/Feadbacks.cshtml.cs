14317b2a0e8d3705ba42040336eb47c664a9bb4b
Kaio Matias Melo <matdantas89@gmail.com>
2024-12-09 17:29:37 -0300

    Adicionar arquivos de projeto.

---
+++ Pages/Feadbacks.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Foto.Pages
+{
+    public class FeadbacksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Loja.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Foto.Pages
+{
+    public class LojaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
